bb98411b99df20e5ec4f1ced64b01b59ea74c3c0
Eldar Aliev <gontrollerraids@gmail.com>
2023-11-26 23:46:33 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <img src="/Images/pngwing.com.png" alt="" />
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link showbig" href="main.html">
+                            Home
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="teams.html">
+                            Teams
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="drivers.html">
+                            Drivers
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            {/* Images for the front page */}
+            {/* Note: In React, use the 'src' attribute directly for image paths */}
+
+            {/* Bootstrap JS dependencies */}
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+            <script
+                src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"
+                defer
+            ></script>
+            <script
+                src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+                defer
+            ></script>
+        </nav>
+
+    
   );
 }
 
